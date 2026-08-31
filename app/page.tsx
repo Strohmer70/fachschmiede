fd8fedbf3d4002dca3001fb5cbcb2ef8c167e57b
Finn <hello@fachschmiede.de>
2026-08-31 19:02:25 +0800

    fix: ESLint config + suppress dangerouslySetInnerHTML warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
+// eslint-disable-next-line @next/next/no-img-element
 export default function HomePage() {
   const html = readFileSync(join(process.cwd(), 'public', 'start.html'), 'utf-8')
+  // eslint-disable-next-line react/no-danger
   return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
